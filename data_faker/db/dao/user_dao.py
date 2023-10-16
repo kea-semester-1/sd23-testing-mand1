bb03882565973447f9fd3d808ffd21ff96f259c7
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2023-10-16 21:17:29 +0200

    factories and create random user api

--- data_faker/db/dao/user_dao.py
+++ data_faker/db/dao/user_dao.py
@@ -2,11 +2,9 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from data_faker.db.dependencies import get_db_session
-from data_faker.db.models.models import User, Address
+from data_faker.db.models.models import User
 import sqlalchemy as sa
-from datetime import datetime
-from typing import Any
-import factory 
+from data_faker.db.factories import UserFactory, AddressFactory
 
 
 class UserDAO:
@@ -17,24 +15,26 @@ class UserDAO:
 
     async def create(self) -> None:  # TODO: Return id
         """Create function for User DAO."""
-        address = Address(
-            street="Gade",
-            number="26",
-            floor="5",
-            door="3",
-            town="København",
-            postal_code=2,
-        )
+        address = AddressFactory()
+        # address = Address(
+        #     street="Gade",
+        #     number="26",
+        #     floor="5",
+        #     door="3",
+        #     town="København",
+        #     postal_code=2,
+        # )
 
-        user = User(
-            first_name="Malthe",
-            last_name="Gram",
-            gender="male",
-            cpr="22222222",
-            date_of_birth=datetime.now(),
-            address=address,
-            phone_number="22222",
-        )
+        user = UserFactory()
+        # user = User(
+        #     first_name="Malthe",
+        #     last_name="Gram",
+        #     gender="male",
+        #     cpr="22222222",
+        #     date_of_birth=datetime.now(),
+        #     address=address,
+        #     phone_number="22222",
+        # )
 
         self.session.add(address)
         await self.session.commit()
--- data_faker/web/api/users/views.py
+++ data_faker/web/api/users/views.py
@@ -1,27 +1,13 @@
 from fastapi import APIRouter, Depends
 from data_faker.db.dao.user_dao import UserDAO
-from data_faker.db.models.models import User
-from pydantic import BaseModel
-from datetime import datetime
 
 router = APIRouter()
 
 
-class UserDTO(BaseModel):
-    first_name: str
-    last_name: str
-    gender: str
-    cpr: str
-    date_of_birth: datetime
-    phone_number: str
-    address: str
-
-
 @router.get("")
-async def get_users(dao: UserDAO = Depends()) -> list[UserDTO]:
+async def get_users(dao: UserDAO = Depends()):
     """Get all users."""
-    users = await dao.get_all()
-    return [UserDTO.model_validate(user) for user in users]
+    return await dao.get_all()
 
 
 @router.post("")
