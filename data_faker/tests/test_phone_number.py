42e615c40b4750107d14a9c5e5bcab8ca174c198
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2023-10-17 18:48:35 +0200

    more tests

--- data_faker/tests/test_phone_number.py
+++ data_faker/tests/test_phone_number.py
@@ -100,8 +100,40 @@ valid_prefixes = [
 
 
 @pytest.mark.parametrize("prefix", valid_prefixes)
-def test_valid_mobile_numbers(prefix: list[str]) -> None:
-    """Test a valid phone number."""
+def test_valid_phone_numbers_lower_boundary(prefix: list[str]) -> None:
+    """Test a valid phone number for lower boundary partition."""
     number = str(prefix) + "0" * (8 - len(prefix))
     print(number)
     assert is_valid_phone_number(number)
+
+
+@pytest.mark.parametrize("prefix", valid_prefixes)
+def test_valid_phone_numbers_upper_boundary(prefix: str) -> None:
+    """Test a valid phone number for lower boundary partition."""
+    number = prefix + "9" * (8 - len(prefix))
+    print(number)
+    assert is_valid_phone_number(number)
+
+
+invalid_numbers = [
+    # Numbers less than 8 digits
+    "3000000",
+    # Numbers more than 8 digits
+    "300000000",
+    # Numbers not starting with specified combinations
+    "33000000",
+    "34300000",
+    "35000000",
+    "54000000",
+    "66600000",  # Number of the beast
+    # Numbers with characters other than digits
+    "3000A000",
+    # Numbers with leading zeroes
+    "03000000",
+]
+
+
+@pytest.mark.parametrize("number", invalid_numbers)
+def test_invalid_phone_numbers(number: str) -> None:
+    """Test invalid phone numbers."""
+    assert not is_valid_phone_number(number)
