c8b0002e0bb7e916afa50d9bed44245d6a5007f0
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2023-10-17 10:50:19 +0200

    added endpoint for getting all the the information

--- data_faker/db/dao/user_dao.py
+++ data_faker/db/dao/user_dao.py
@@ -2,7 +2,7 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from data_faker.db.dependencies import get_db_session
-from data_faker.db.models.models import User
+from data_faker.db.models.models import User, Address
 import sqlalchemy as sa
 from data_faker.db.factories import UserFactory, AddressFactory
 
@@ -32,3 +32,33 @@ class UserDAO:
         users = await self.session.execute(query)
 
         return list(users.scalars().fetchall())
+
+    async def get_all_with_address(self) -> list[dict]:
+        """Get all users with their addresses."""
+
+        query = sa.select(User, Address).join(Address, User.address_id == Address.id)
+        result = await self.session.execute(query)
+        rows = result.fetchall()
+
+        combined = []
+        for user, address in rows:
+            user_data = {
+                "id": user.id,
+                "gender": user.gender,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "cpr": user.cpr,
+                "date_of_birth": str(user.date_of_birth),
+                "phone_number": user.phone_number,
+                "address_id": str(user.address_id),
+                "address": {
+                    "street": address.street,
+                    "number": address.number,
+                    "floor": address.floor,
+                    "town": address.town,
+                    "postal_code": address.postal_code,
+                },
+            }
+            combined.append(user_data)
+
+        return combined
