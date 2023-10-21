b6564ff414c4945c0b4c3ac3df73355f50a0a27c
Moshizzl3 <81045658+Moshizzl3@users.noreply.github.com>
2023-10-21 13:26:48 +0200

    added more tests

--- data_faker/tests/test_door.py
+++ data_faker/tests/test_door.py
@@ -1,35 +1,41 @@
 import pytest
-from data_faker.db.mo_utils import (
-    validate_door_value,
-)  # Adjust the import according to your module setup.
+from data_faker.db.mo_utils import validate_door_value
 
 
 @pytest.mark.parametrize(
     "value,expected",
     [
-        # Test for predefined strings
+        # Test for predefined floors
         ("th", True),
         ("mf", True),
         ("tv", True),
-        ("abc", False),  # Random string
+        ("xy", False),  # Random string
         ("tvf", False),  # Too long predefined string
+        ("f", False),  # Too short predefined string
         # Test for numbers from 1 to 50
         ("1", True),
         ("25", True),
-        ("50", True),
-        ("51", False),
-        ("0", False),
-        ("-1", False),
-        # Test for lowercase letter optionally followed by a dash and then one to three numeric digits
+        ("0", False),  # test for zero
+        ("-1", False),  # test for negative number
+        # Boundry partitons
+        ("49", True),  # test for upper boundary valid partition
+        ("50", True),  # test for highest value possible
+        ("51", False),  # test for upper boundary invalid partition
+        # Test for lowercase letter optionally followed by a dash
+        # and then one to three numeric digits
         ("a1", True),
         ("a-1", True),
+        ("z99", True),
+        ("z99", True),
         ("z999", True),
         ("z-999", True),
         ("0a", False),  # Number starts with 0
         ("ab1", False),  # Two letters
         ("0a-01", False),  # Leading zeros
         ("a-1000", False),  # Too many digits
+        ("a-", False),  # No integers after the dash
     ],
 )
-def test_validate_door_value(value, expected):
+def test_validate_door_value(value: str, expected: bool) -> None:
+    """Test the validate door function."""
     assert validate_door_value(value) == expected
