949e74a5d759cc809c56a03c23a7abd4cedbfb93
martinlaursen97 <martin_laursen21@hotmail.com>
2023-10-22 17:35:48 +0200

    doc

--- data_faker/web/api/faker/views.py
+++ data_faker/web/api/faker/views.py
@@ -9,13 +9,13 @@ router = APIRouter()
 
 @router.get("/single")
 def create_single() -> FakeInfoDTO:
-    """Create fake info batch demo."""
+    """Create fake info."""
 
     return factories.FakeInfoFactory.create()
 
 
 @router.get("/batch")
 def create_batch(size: Annotated[int, Query(ge=1, le=100)]) -> list[FakeInfoDTO]:
-    """Create fake info batch demo."""
+    """Create fake info in batch."""
 
     return factories.FakeInfoFactory.create_batch(size)
